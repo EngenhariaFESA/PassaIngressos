88dd13249e385bf2aa1ec80826dbc198bf228a77
Renan Cesar De Araujo <101188631+RenanCesarDeAraujo@users.noreply.github.com>
2024-10-15 14:24:10 -0300

    comentei 2 testes na AcessoTests

--- PassaIngressos_WebAPI.Tests/obj/Release/net6.0/PassaIngressos_WebAPI.Tests.AssemblyInfo.cs
+++ PassaIngressos_WebAPI.Tests/obj/Release/net6.0/PassaIngressos_WebAPI.Tests.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PassaIngressos_WebAPI.Tests")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0467b824a3b9eeb30bbfbb4dab4742ab9e093ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PassaIngressos_WebAPI.Tests")]
